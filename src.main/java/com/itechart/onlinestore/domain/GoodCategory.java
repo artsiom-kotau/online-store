b4c89a875d0d235a4b16439b152022c3beba907b
iryna.karzhova <Iryna.Karzhova@itechart-group.com>
2021-04-14 22:39:01 +0300

    Added domains.

---
+++ src.main/java/com/itechart/onlinestore/domain/GoodCategory.java
@@ -0,0 +1,8 @@
+package com.itechart.onlinestore.domain;
+
+import java.util.UUID;
+
+public class GoodCategory {
+    private UUID goodId;
+    private UUID categoryId;
+}
---
+++ src.main/java/com/itechart/onlinestore/domain/User.java
@@ -0,0 +1,11 @@
+package com.itechart.onlinestore.domain;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+
+    private Profile profile;
+
+    private Bucket bucket;
+}
